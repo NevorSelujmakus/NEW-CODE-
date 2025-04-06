dd4e1a54d6e82edae0e3c6c7b48a8202cb8fc3db
NevorSelujmakus <156199274+NevorSelujmakus@users.noreply.github.com>
2025-04-06 13:20:41 +0000

    This is for notes

---
+++ ascii.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "___  ___  _______   ___       ___       ________                 \n";
+    cout << "|\\  \\\\  \\\\  ___ \\\\ |\\  \\     |\\  \\     |\\   __  \\                \n";
+    cout << "\\ \\  \\\\  \\\\ \\   __/|\\ \\  \\    \\ \\  \\    \\ \\  \\\\  \\               \n";
+    cout << " \\ \\   __  \\ \\  \\_|/_\\ \\  \\    \\ \\  \\    \\ \\  \\\\  \\              \n";
+    cout << "  \\ \\  \\ \\  \\ \\  \\_|\\ \\ \\  \\____\\ \\  \\____\\ \\  \\\\  \\             \n";
+    cout << "   \\ \\__\\ \\__\\ \\_______\\ \\_______\\ \\_______\\ \\_______\\            \n";
+    cout << "    \\|__|\\|__|\\|_______|\\|_______|\\|_______|\\|_______|            \n";
+    cout << "                                                                  \n";
+    cout << "                                                                  \n";
+    cout << "                                                                  \n";
+    cout << " ___       __   ________  ________  ___       ________  ___       \n";
+    cout << "|\\  \\     |\\  \\|\\   __  \\|\\   __  \\|\\  \\     |\\   ___ \\\\|\\  \\      \n";
+    cout << "\\ \\  \\    \\ \\  \\\\ \\  \\\\  \\ \\  \\\\  \\ \\  \\    \\ \\  \\_|\\ \\ \\  \\     \n";
+    cout << " \\ \\  \\  __\\ \\  \\ \\  \\\\  \\ \\   _  _\\ \\  \\    \\ \\  \\ \\\\ \\ \\  \\    \n";
+    cout << "  \\ \\  \\|__\\_\\  \\ \\  \\\\  \\ \\  \\\\  \\\\ \\  \\____\\ \\  \\_\\ \\ \\__\\   \n";
+    cout << "   \\ \\____________\\ \\_______\\ \\__\\ _\\ \\_______\\ \\_______\\|__|   \n";
+    cout << "    \\|____________|\\|_______|\\|__|\\|__|\\|_______|\\|_______|   ___ \n";
+    cout << "                                                             |\\__\\\n";
+    cout << "                                                             \\|__|\n";                                                        
+    cout << "              _-o#&&*''''?d:>b\\_\n";
+    cout << "          _o/\"`''  '',, dMF9MMMMMHo_\n";
+    cout << "       .o&#'        `\"MbHMMMMMMMMMMMHo.\n";
+    cout << "     .o\"\" '         vodM*$&&HMMMMMMMMMM?.\n";
+    cout << "    ,'              $M&ood,~'`(&##MMMMMMH\\\n";
+    cout << "   /               ,MMMMMMM#b?#bobMMMMHMMML\n";
+    cout << "  &              ?MMMMMMMMMMMMMMMMM7MMM$R*Hk\n";
+    cout << " ?$.            :MMMMMMMMMMMMMMMMMMM/HMMM|`*L\n";
+    cout << "|               |MMMMMMMMMMMMMMMMMMMMbMH'   T,\n";
+    cout << "$H#:            `*MMMMMMMMMMMMMMMMMMMMb#}'  `?\n";
+    cout << "]MMH#             \"\"*\"\"\"\"*#MMMMMMMMMMMMM'    -\n";
+    cout << "MMMMMb_                   |MMMMMMMMMMMP'     :\n";
+    cout << "HMMMMMMMHo                 `MMMMMMMMMT       .\n";
+    cout << "?MMMMMMMMP                  9MMMMMMMM}       -\n";
+    cout << "-?MMMMMMM                  |MMMMMMMMM?,d-    '\n";
+    cout << " :|MMMMMM-                 `MMMMMMMT .M|.   :\n";
+    cout << "  .9MMM[                    &MMMMM*' `'    .\n";
+    cout << "   :9MMk                    `MMM#\"        -\n";
+    cout << "     &M}                     `          .-\n";
+    cout << "      `&.                             .\n";
+    cout << "        `~,   .                     ./\n";
+    cout << "            . _                  .-\n";
+    cout << "              '`--._,dd###pp=\"\"'\n";
+    
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,69 @@
 #include <iostream>
+#include <stack>
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    cout << "BOSS TY MALAKAS" << endl;
-    //comment
+int main(){
+    stack<string> mystack;
+    char orders[50];
+    char input = ' ';
+    cout << "[A] Push Data" << endl;
+    cout << "[B] Pop Data" << endl;
+    cout << "[C] show top" << endl;
+    cout << "[D] show size" << endl;
+    cout << "[E] Exit" << endl;
+    cout << "[F] Show all orders" << endl;
+    
+
+    while (input != 'E'){
+
+        cout << "Enter your choice: ";
+        cin >> input;
+        
+        switch (input){
+            case 'A':
+                cout << "Enter your order: ";
+                cin >> orders;
+                mystack.push(orders);
+                break;
+            case 'B':
+                if (mystack.empty()){
+                    cout << "Stack is empty" << endl;
+                } else {
+                    cout << "Data " << mystack.top() << " has been removed" << endl;
+                    mystack.pop();
+                }
+                break;
+            case 'C':
+                if (mystack.empty()){
+                    cout << "Stack is empty" << endl;
+                } else {
+                    cout << "Top data: " << mystack.top() << endl;
+                }
+                break;
+            case 'D':
+                cout << "Size of stack: " << mystack.size() << endl;
+                break;
+            case 'E':
+                cout << "Exiting program" << endl;
+                break;
+            case 'F':
+                if (mystack.empty()){
+                    cout << "Stack is empty" << endl;
+                }
+                else{
+                    int x=1;
+                    while (!mystack.empty()){
+                        cout << "Order(" << x << "): " << mystack.top() << endl;
+                        mystack.pop();
+                        x++;} 
+                }
+                break;     
+            default:
+                cout << "Invalid input" << endl;
+                break;
+        }
+        
+    }
     return 0;
 }
